cea37cd48978b3d11e81cbf72a0f7d82597f40a4
ryosuketc <ryosuke.aria61@gmail.com>
2020-09-08 14:31:48 +0900

    Extract long table-like variables to another file

--- latexify/core.py
+++ latexify/core.py
@@ -20,16 +20,9 @@ import inspect
 
 import dill
 
-_MATH_SYMBOLS = {
-    'aleph', 'alpha', 'beta', 'beth', 'chi', 'daleth',
-    'delta', 'digamma', 'epsilon', 'eta', 'gamma', 'gimel',
-    'iota', 'kappa', 'lambda', 'mu', 'nu', 'omega', 'omega',
-    'phi', 'pi', 'psi', 'rho', 'sigma', 'tau', 'theta',
-    'upsilon', 'varepsilon', 'varkappa', 'varphi', 'varpi', 'varrho',
-    'varsigma', 'vartheta', 'xi', 'zeta', 'Delta', 'Gamma',
-    'Lambda', 'Omega', 'Phi', 'Pi', 'Sigma', 'Theta',
-    'Upsilon', 'Xi',
-}
+from latexify import predefined_variables
+
+_MATH_SYMBOLS = predefined_variables.get_math_symbols()
 
 class LatexifyVisitor(ast.NodeVisitor):
 
@@ -70,34 +63,7 @@ class LatexifyVisitor(ast.NodeVisitor):
     return r'\left\{ ' + r'\space,\space '.join([self.visit(i) for i in node.elts]) + r'\right\} '
 
   def visit_Call(self, node):
-    builtin_callees = {
-        'abs': (r'\left|{', r'}\right|'),
-        'math.acos': (r'\arccos{\left({', r'}\right)}'),
-        'math.acosh': (r'\mathrm{arccosh}{\left({', r'}\right)}'),
-        'math.asin': (r'\arcsin{\left({', r'}\right)}'),
-        'math.asinh': (r'\mathrm{arcsinh}{\left({', r'}\right)}'),
-        'math.atan': (r'\arctan{\left({', r'}\right)}'),
-        'math.atanh': (r'\mathrm{arctanh}{\left({', r'}\right)}'),
-        'math.ceil': (r'\left\lceil{', r'}\right\rceil'),
-        'math.cos': (r'\cos{\left({', r'}\right)}'),
-        'math.cosh': (r'\cosh{\left({', r'}\right)}'),
-        'math.exp': (r'\exp{\left({', r'}\right)}'),
-        'math.fabs': (r'\left|{', r'}\right|'),
-        'math.factorial': (r'\left({', r'}\right)!'),
-        'math.floor': (r'\left\lfloor{', r'}\right\rfloor'),
-        'math.fsum': (r'\sum\left({', r'}\right)'),
-        'math.gamma': (r'\Gamma\left({', r'}\right)'),
-        'math.log': (r'\log{\left({', r'}\right)}'),
-        'math.log10': (r'\log_{10}{\left({', r'}\right)}'),
-        'math.log2': (r'\log_{2}{\left({', r'}\right)}'),
-        'math.prod': (r'\prod \left({', r'}\right)'),
-        'math.sin': (r'\sin{\left({', r'}\right)}'),
-        'math.sinh': (r'\sinh{\left({', r'}\right)}'),
-        'math.sqrt': (r'\sqrt{', '}'),
-        'math.tan': (r'\tan{\left({', r'}\right)}'),
-        'math.tanh': (r'\tanh{\left({', r'}\right)}'),
-        'sum': (r'\sum \left({', r'}\right)'),
-    }
+    builtin_callees = predefined_variables.get_builtin_callees()
 
     callee_str = self.visit(node.func)
     if callee_str in builtin_callees:
@@ -146,16 +112,7 @@ class LatexifyVisitor(ast.NodeVisitor):
       return r'\mathrm{unknown\_uniop}(' + self.visit(node.operand) + ')'
 
   def visit_BinOp(self, node):
-    priority = {
-        ast.Add: 10,
-        ast.Sub: 10,
-        ast.Mult: 20,
-        ast.MatMult: 20,
-        ast.Div: 20,
-        ast.FloorDiv: 20,
-        ast.Mod: 20,
-        ast.Pow: 30,
-    }
+    priority = predefined_variables.get_BinOp_priority()
 
     def _unwrap(child):
       return self.visit(child)
