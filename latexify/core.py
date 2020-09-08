8c86649f96544508e960df51f96cd88f35c074ec
Shigeki Karita <karita@google.com>
2020-09-08 10:52:19 +0900

    Add pylint, and fix errors and warnings.

--- latexify/core.py
+++ latexify/core.py
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 # This is very scratchy and supports only limited portion of Python functions.
+"""Latexify core module."""
 
 import ast
-import math
 import inspect
 
 import dill
@@ -32,28 +32,30 @@ _MATH_SYMBOLS = {
 }
 
 class LatexifyVisitor(ast.NodeVisitor):
+  """Latexify AST visitor."""
 
   def __init__(self, math_symbol):
     self.math_symbol = math_symbol
-    super(ast.NodeVisitor).__init__()
+    super().__init__()
 
   def _parse_math_symbols(self, val: str) -> str:
     if not self.math_symbol:
       return val
     if val in _MATH_SYMBOLS:
       return '{\\' + val + '}'
-    else:
-      return val
+    return val
 
   def generic_visit(self, node):
     return str(node)
 
+  # pylint: disable=invalid-name
   def visit_Module(self, node):
     return self.visit(node.body[0])
 
   def visit_FunctionDef(self, node):
     name_str = r'\mathrm{' + str(node.name) + '}'
-    arg_strs = [self._parse_math_symbols(str(arg.arg)) for arg in node.args.args]
+    arg_strs = [
+        self._parse_math_symbols(str(arg.arg)) for arg in node.args.args]
     body_str = self.visit(node.body[0])
     return name_str + '(' + ', '.join(arg_strs) + r') \triangleq ' + body_str
 
@@ -61,15 +63,19 @@ class LatexifyVisitor(ast.NodeVisitor):
     return self.visit(node.value)
 
   def visit_Tuple(self, node):
-    return r'\left( ' + r'\space,\space '.join([self.visit(i) for i in node.elts]) + r'\right) '
+    elts = [self.visit(i) for i in node.elts]
+    return r'\left( ' + r'\space,\space '.join(elts) + r'\right) '
 
   def visit_List(self, node):
-    return r'\left[ ' + r'\space,\space '.join([self.visit(i) for i in node.elts]) + r'\right] '
+    elts = [self.visit(i) for i in node.elts]
+    return r'\left[ ' + r'\space,\space '.join(elts) + r'\right] '
 
   def visit_Set(self, node):
-    return r'\left\{ ' + r'\space,\space '.join([self.visit(i) for i in node.elts]) + r'\right\} '
+    elts = [self.visit(i) for i in node.elts]
+    return r'\left\{ ' + r'\space,\space '.join(elts) + r'\right\} '
 
   def visit_Call(self, node):
+    """Visit a call node."""
     builtin_callees = {
         'abs': (r'\left|{', r'}\right|'),
         'math.acos': (r'\arccos{\left({', r'}\right)}'),
@@ -128,9 +134,11 @@ class LatexifyVisitor(ast.NodeVisitor):
     return str(node.n)
 
   def visit_UnaryOp(self, node):
+    """Visit a unary op node."""
     def _wrap(child):
       latex = self.visit(child)
-      if isinstance(child, ast.BinOp) and isinstance(child.op, (ast.Add, ast.Sub)):
+      if (isinstance(child, ast.BinOp) and
+          isinstance(child.op, (ast.Add, ast.Sub))):
         return r'\left(' + latex + r'\right)'
       return latex
 
@@ -142,10 +150,10 @@ class LatexifyVisitor(ast.NodeVisitor):
 
     if type(node.op) in reprs:
       return reprs[type(node.op)]()
-    else:
-      return r'\mathrm{unknown\_uniop}(' + self.visit(node.operand) + ')'
+    return r'\mathrm{unknown\_uniop}(' + self.visit(node.operand) + ')'
 
   def visit_BinOp(self, node):
+    """Visit a binary op node."""
     priority = {
         ast.Add: 10,
         ast.Sub: 10,
@@ -184,39 +192,40 @@ class LatexifyVisitor(ast.NodeVisitor):
 
     if type(node.op) in reprs:
       return reprs[type(node.op)]()
-    else:
-      return r'\mathrm{unknown\_binop}(' + _unwrap(l) + ', ' + _unwrap(r) + ')'
+    return r'\mathrm{unknown\_binop}(' + _unwrap(l) + ', ' + _unwrap(r) + ')'
 
   def visit_Compare(self, node):
+    """Visit a compare node."""
     lstr = self.visit(node.left)
     rstr = self.visit(node.comparators[0])
 
     if isinstance(node.ops[0], ast.Eq):
       return lstr + '=' + rstr
-    elif isinstance(node.ops[0], ast.Gt):
+    if isinstance(node.ops[0], ast.Gt):
       return lstr + '>' + rstr
-    elif isinstance(node.ops[0], ast.Lt):
+    if isinstance(node.ops[0], ast.Lt):
       return lstr + '<' + rstr
-    elif isinstance(node.ops[0], ast.GtE):
+    if isinstance(node.ops[0], ast.GtE):
       return lstr + r'\ge ' + rstr
-    elif isinstance(node.ops[0], ast.LtE):
+    if isinstance(node.ops[0], ast.LtE):
       return lstr + r'\le ' + rstr
-    elif isinstance(node.ops[0], ast.NotEq):
+    if isinstance(node.ops[0], ast.NotEq):
       return lstr + r'\ne ' + rstr
-    elif isinstance(node.ops[0], ast.Is):
+    if isinstance(node.ops[0], ast.Is):
       return lstr + r'\equiv' + rstr
 
-    else:
-      return r'\mathrm{unknown\_comparator}(' + lstr + ', ' + rstr + ')'
+    return r'\mathrm{unknown\_comparator}(' + lstr + ', ' + rstr + ')'
 
   def visit_BoolOp(self, node):
     logic_operator = r'\lor ' if isinstance(node.op, ast.Or) \
                 else r'\land ' if isinstance(node.op, ast.And) \
                 else r' \mathrm{unknown\_operator} '
     # visit all the elements in the ast.If node recursively
-    return r'\left('+self.visit(node.values[0])+r'\right)'+logic_operator+r'\left('+self.visit(node.values[1])+r'\right)'
+    return (r'\left(' + self.visit(node.values[0]) + r'\right)' + logic_operator
+            + r'\left(' + self.visit(node.values[1]) + r'\right)')
 
   def visit_If(self, node):
+    """Visit an if node."""
     latex = r'\left\{ \begin{array}{ll} '
 
     while isinstance(node, ast.If):
@@ -225,12 +234,14 @@ class LatexifyVisitor(ast.NodeVisitor):
       latex += true_latex + r', & \mathrm{if} \ ' + cond_latex + r' \\ '
       node = node.orelse[0]
 
-    return latex + self.visit(node) + r', & \mathrm{otherwise} \end{array} \right.'
+    latex += self.visit(node)
+    return latex + r', & \mathrm{otherwise} \end{array} \right.'
 
 
 def get_latex(fn, math_symbol=True):
   try:
     source = inspect.getsource(fn)
+  # pylint: disable=broad-except
   except Exception:
     # Maybe running on console.
     source = dill.source.getsource(fn)
@@ -239,8 +250,11 @@ def get_latex(fn, math_symbol=True):
 
 
 def with_latex(*args, math_symbol=True):
+  """Translate a function with latex representation."""
 
   class _LatexifiedFunction:
+    """Function with latex representation."""
+
     def __init__(self, fn):
       self._fn = fn
       self._str = get_latex(fn, math_symbol=math_symbol)
@@ -275,5 +289,7 @@ def with_latex(*args, math_symbol=True):
 
   if len(args) == 1 and callable(args[0]):
     return _LatexifiedFunction(args[0])
-  else:
-    return lambda fn: _LatexifiedFunction(fn)
+
+  def ret(fn):
+    return _LatexifiedFunction(fn)
+  return ret
