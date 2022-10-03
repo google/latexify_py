6462b0cc57fcd388e0f09cfc8b7e5ef6ead73fc1
William Jawad <wiljav@users.noreply.github.com>
2022-10-03 06:04:25 +0300

    Updated to avoid support to Python version 3.11
    Co-authored-by: Yusuke Oda <yusuke.oda@predicate.jp>

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ def main():
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Software Development :: Code Generators',
         'Topic :: Text Processing :: Markup :: LaTeX',
