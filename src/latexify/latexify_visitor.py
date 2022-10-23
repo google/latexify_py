de6d72d7872f128cf6b35158c4b8cde6d9cc8123
odashi <odashi@inspiredco.ai>
2022-10-23 07:32:20 +0000

    improve boolop

--- src/latexify/latexify_visitor.py
+++ src/latexify/latexify_visitor.py
@@ -260,8 +260,8 @@ class LatexifyVisitor(node_visitor_base.NodeVisitorBase):
         ast.NotIn: r"\notin",
     }
 
-    def visit_Compare(self, node, action):  # pylint: disable=invalid-name
-        """Visit a compare node."""
+    def visit_Compare(self, node: ast.Compare, action) -> str:
+        """Visit a Compare node."""
         if len(node.ops) != 1:
             raise SyntaxError("Multiple compares are not supported.")
 
@@ -269,24 +269,16 @@ class LatexifyVisitor(node_visitor_base.NodeVisitorBase):
         rhs = self.visit(node.comparators[0])
         return f"{{{lhs} {self._compare_ops[type(node.ops[0])]} {rhs}}}"
 
-    def visit_BoolOp(self, node, action):  # pylint: disable=invalid-name
-        logic_operator = (
-            r"\lor "
-            if isinstance(node.op, ast.Or)
-            else r"\land "
-            if isinstance(node.op, ast.And)
-            else r" \mathrm{unknown\_operator} "
-        )
-        # visit all the elements in the ast.If node recursively
-        return (
-            r"\left("
-            + self.visit(node.values[0])
-            + r"\right)"
-            + logic_operator
-            + r"\left("
-            + self.visit(node.values[1])
-            + r"\right)"
-        )
+    _bool_ops: ClassVar[dict[type[ast.boolop], str]] = {
+        ast.And: r"\land",
+        ast.Or: r"\lor",
+    }
+
+    def visit_BoolOp(self, node: ast.BoolOp, action) -> str:
+        """Visit a BoolOp node."""
+        values = [rf"\left( {self.visit(x)} \right)" for x in node.values]
+        op = f" {self._bool_ops[type(node.op)]} "
+        return "{" + op.join(values) + "}"
 
     def visit_If(self, node, action):  # pylint: disable=invalid-name
         """Visit an if node."""
--- src/latexify/latexify_visitor_test.py
+++ src/latexify/latexify_visitor_test.py
@@ -32,3 +32,24 @@ def test_visit_compare_multiple() -> None:
     assert isinstance(tree, ast.Compare)
     with pytest.raises(SyntaxError, match=r"^Multiple compares are not supported\.$"):
         LatexifyVisitor().visit(tree)
+
+
+@pytest.mark.parametrize(
+    "code,latex",
+    [
+        ("a and b", r"{\left( a \right) \land \left( b \right)}"),
+        (
+            "a and b and c",
+            r"{\left( a \right) \land \left( b \right) \land \left( c \right)}",
+        ),
+        ("a or b", r"{\left( a \right) \lor \left( b \right)}"),
+        (
+            "a or b or c",
+            r"{\left( a \right) \lor \left( b \right) \lor \left( c \right)}",
+        ),
+    ],
+)
+def test_visit_boolop(code: str, latex: str) -> None:
+    tree = ast.parse(code).body[0].value
+    assert isinstance(tree, ast.BoolOp)
+    assert LatexifyVisitor().visit(tree) == latex
