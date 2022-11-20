df5a64ec3e4b971fd5ac5713da7c0b826c2b9f7f
Zibing Zhang <zizhang@hubspot.com>
2022-11-20 10:50:03 +0000

    Reformat import

--- src/latexify/transformers/__init__.py
+++ src/latexify/transformers/__init__.py
@@ -1,7 +1,10 @@
 """Package latexify.transformers."""
 
-from latexify.transformers import (assignment_reducer, function_expander,
-                                   identifier_replacer)
+from latexify.transformers import (
+    assignment_reducer,
+    function_expander,
+    identifier_replacer,
+)
 
 AssignmentReducer = assignment_reducer.AssignmentReducer
 FunctionExpander = function_expander.FunctionExpander
