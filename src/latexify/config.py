32851eee6003ddbd06ec3569a198932d8e70bdfb
chunibyo <chunibyo.wly@gmail.com>
2022-11-21 23:08:37 +0800

    [fix] documentation

--- src/latexify/config.py
+++ src/latexify/config.py
@@ -9,7 +9,24 @@ from typing import Any
 
 @dataclasses.dataclass(frozen=True)
 class Config:
-    """Configurations to control the behavior of latexify."""
+    """Configurations to control the behavior of latexify.
+
+    Attributes:
+        identifiers: If set, the mapping to replace identifier names in the
+            function. Keys are the original names of the identifiers,
+            and corresponding values are the replacements.
+            Both keys and values have to represent valid Python identifiers:
+            ^[A-Za-z_][A-Za-z0-9_]*$
+        reduce_assignments: If True, assignment statements are used to synthesize
+            the final expression.
+        use_math_symbols: Whether to convert identifiers with a math symbol surface
+            (e.g., "alpha") to the LaTeX symbol (e.g., "\\alpha").
+        use_raw_function_name: Whether to keep underscores "_" in the function name,
+            or convert it to subscript.
+        use_signature: Whether to add the function signature before the expression
+            or not.
+        use_set_symbols: Whether to use set symbols or not.
+    """
 
     identifiers: dict[str, str] | None
     reduce_assignments: bool
@@ -45,7 +62,7 @@ class Config:
     @staticmethod
     def defaults() -> Config:
         """Generates a Config with default values.
-        
+
         Returns:
             A new Config with default values
         """
--- src/latexify/frontend.py
+++ src/latexify/frontend.py
@@ -20,7 +20,8 @@ def get_latex(fn: Callable[..., Any], *, config: Config | None = None, **kwargs)
         fn: Reference to a function to analyze.
         config: use defined Config object, if it is None, it will be automatic assigned
             with default value.
-        **kwargs: dict of Config field values that could be defined individually by uses.
+        **kwargs: dict of Config field values that could be defined individually
+            by users.
 
     Returns:
         Generatee LaTeX description.
