a2bf5248544f2c2705d8ffbb7ff20d98cfb21948
Zibing Zhang <zizhang@hubspot.com>
2022-12-11 11:34:31 +0000

    codegen

--- src/integration_tests/algorithmic_style_test.py
+++ src/integration_tests/algorithmic_style_test.py
@@ -48,6 +48,27 @@ def test_factorial() -> None:
     check_algorithm(fact, latex)
 
 
+def test_factorial_jupyter() -> None:
+    def fact(n):
+        if n == 0:
+            return 1
+        else:
+            return n * fact(n - 1)
+
+    latex = (
+        r"\displaystyle \hspace{0pt} \mathbf{function} \ \mathrm{FACT}(n): \\"
+        r" \displaystyle \hspace{16pt}\mathbf{if} \ n = 0 \\"
+        r" \displaystyle \hspace{32pt} \mathbf{return} \ 1 \\"
+        r" \displaystyle \hspace{16pt}\mathbf{else} \\"
+        r" \displaystyle \hspace{32pt}"
+        r" \mathbf{return} \ n"
+        r" \mathrm{fact} \mathopen{}\left( n - 1 \mathclose{}\right) \\"
+        r" \displaystyle \hspace{16pt} \mathbf{end \ if} \\"
+        r" \displaystyle \hspace{0pt} \mathbf{end \ function}"
+    )
+    check_algorithm(fact, latex, environment=frontend.Environment.JUPYTER_NOTEBOOK)
+
+
 def test_collatz() -> None:
     def collatz(n):
         iterations = 0
@@ -75,3 +96,32 @@ def test_collatz() -> None:
         r" \end{algorithmic}"
     )
     check_algorithm(collatz, latex)
+
+
+def test_collatz_jupyter() -> None:
+    def collatz(n):
+        iterations = 0
+        while n > 1:
+            if n % 2 == 0:
+                n = n // 2
+            else:
+                n = 3 * n + 1
+            iterations = iterations + 1
+        return iterations
+
+    latex = (
+        r"\displaystyle \hspace{0pt} \mathbf{function} \ \mathrm{COLLATZ}(n): \\"
+        r" \displaystyle \hspace{16pt} \mathrm{iterations} \gets 0 \\"
+        r" \displaystyle \hspace{16pt} \mathbf{while} \ n > 1 \\"
+        r" \displaystyle \hspace{32pt}\mathbf{if} \ n \mathbin{\%} 2 = 0 \\"
+        r" \displaystyle \hspace{48pt} n \gets \left\lfloor\frac{n}{2}\right\rfloor \\"
+        r" \displaystyle \hspace{32pt}\mathbf{else} \\"
+        r" \displaystyle \hspace{48pt} n \gets 3 n + 1 \\"
+        r" \displaystyle \hspace{32pt} \mathbf{end \ if} \\"
+        r" \displaystyle \hspace{32pt}"
+        r" \mathrm{iterations} \gets \mathrm{iterations} + 1 \\"
+        r" \displaystyle \hspace{16pt} \mathbf{end \ while} \\"
+        r" \displaystyle \hspace{16pt} \mathbf{return} \ \mathrm{iterations} \\"
+        r" \displaystyle \hspace{0pt} \mathbf{end \ function}"
+    )
+    check_algorithm(collatz, latex, environment=frontend.Environment.JUPYTER_NOTEBOOK)
--- src/latexify/codegen/algorithmic_codegen.py
+++ src/latexify/codegen/algorithmic_codegen.py
@@ -60,6 +60,7 @@ class AlgorithmicCodegen(ast.NodeVisitor):
         ]
         # Body
         body_strs: list[str] = [self.visit(stmt) for stmt in node.body]
+
         return (
             rf"\begin{{algorithmic}}"
             rf" \Function{{{node.name}}}{{${', '.join(arg_strs)}$}}"
@@ -73,7 +74,6 @@ class AlgorithmicCodegen(ast.NodeVisitor):
         """Visit an If node."""
         cond_latex = self._expression_codegen.visit(node.test)
         body_latex = " ".join(self.visit(stmt) for stmt in node.body)
-
         latex = rf"\If{{${cond_latex}$}} {body_latex}"
 
         if node.orelse:
@@ -113,7 +113,9 @@ class AlgorithmicJupyterCodegen(ast.NodeVisitor):
     LaTeX expression of the given algorithm.
     """
 
+    _PT_PER_INDENT = 16
     _identifier_converter: identifier_converter.IdentifierConverter
+    _indent: int
 
     def __init__(
         self, *, use_math_symbols: bool = False, use_set_symbols: bool = False
@@ -131,3 +133,91 @@ class AlgorithmicJupyterCodegen(ast.NodeVisitor):
         self._identifier_converter = identifier_converter.IdentifierConverter(
             use_math_symbols=use_math_symbols
         )
+        self._indent = 0
+
+    def generic_visit(self, node: ast.AST) -> str:
+        raise exceptions.LatexifyNotSupportedError(
+            f"Unsupported AST: {type(node).__name__}"
+        )
+
+    def visit_Assign(self, node: ast.Assign) -> str:
+        """Visit an Assign node."""
+        operands: list[str] = [
+            self._expression_codegen.visit(target) for target in node.targets
+        ]
+        operands.append(self._expression_codegen.visit(node.value))
+        operands_latex = r" \gets ".join(operands)
+        return rf"{self._prefix()} {operands_latex}"
+
+    def visit_Expr(self, node: ast.Expr) -> str:
+        """Visit an Expr node."""
+        return rf"{self._prefix()} {self._expression_codegen.visit(node.value)}"
+
+    def visit_FunctionDef(self, node: ast.FunctionDef) -> str:
+        """Visit a FunctionDef node."""
+        # Arguments
+        arg_strs = [
+            self._identifier_converter.convert(arg.arg)[0] for arg in node.args.args
+        ]
+        # Body
+        self._indent += 1
+        body_strs: list[str] = [self.visit(stmt) for stmt in node.body]
+        self._indent -= 1
+        body = r" \\ ".join(body_strs)
+
+        return (
+            rf"{self._prefix()} \mathbf{{function}}"
+            rf" \ \mathrm{{{node.name.upper()}}}({', '.join(arg_strs)}): \\"
+            rf" {body} \\"
+            rf" {self._prefix()} \mathbf{{end \ function}}"
+        )
+
+    # TODO(ZibingZhang): support \ELSIF
+    def visit_If(self, node: ast.If) -> str:
+        """Visit an If node."""
+        cond_latex = self._expression_codegen.visit(node.test)
+        self._indent += 1
+        body_latex = r" \\ ".join(self.visit(stmt) for stmt in node.body)
+        self._indent -= 1
+        latex = rf"{self._prefix()}\mathbf{{if}} \ {cond_latex} \\ {body_latex}"
+
+        if node.orelse:
+            latex += rf" \\ {self._prefix()}\mathbf{{else}} \\ "
+            self._indent += 1
+            latex += r" \\ ".join(self.visit(stmt) for stmt in node.orelse)
+            self._indent -= 1
+
+        return latex + rf" \\ {self._prefix()} \mathbf{{end \ if}}"
+
+    def visit_Module(self, node: ast.Module) -> str:
+        """Visit a Module node."""
+        return self.visit(node.body[0])
+
+    def visit_Return(self, node: ast.Return) -> str:
+        """Visit a Return node."""
+        return (
+            rf"{self._prefix()} \mathbf{{return}}"
+            rf" \ {self._expression_codegen.visit(node.value)}"
+            if node.value is not None
+            else rf"{self._prefix()} \mathbf{{return}}"
+        )
+
+    def visit_While(self, node: ast.While) -> str:
+        """Visit a While node."""
+        if node.orelse:
+            raise exceptions.LatexifyNotSupportedError(
+                "While statement with the else clause is not supported"
+            )
+
+        cond_latex = self._expression_codegen.visit(node.test)
+        self._indent += 1
+        body_latex = r" \\ ".join(self.visit(stmt) for stmt in node.body)
+        self._indent -= 1
+        return (
+            rf"{self._prefix()} \mathbf{{while}} \ {cond_latex} \\ "
+            rf"{body_latex} \\ "
+            rf"{self._prefix()} \mathbf{{end \ while}}"
+        )
+
+    def _prefix(self) -> str:
+        return rf"\displaystyle \hspace{{{self._indent * self._PT_PER_INDENT}pt}}"
